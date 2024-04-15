8c32a8c01022d6af3bbd5df194150e3e02f81d74
Lep Sistemas <lepsistemas@gmail.com>
2024-04-15 19:59:07 -0300

    Remove fs

--- server/index.js
+++ server/index.js
@@ -2,11 +2,6 @@ import express from 'express';
 import fetch from 'node-fetch';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import fs from 'fs'; // Node.js File System module for saving files
-import { pipeline } from 'stream'; // For handling Node.js streams
-import { promisify } from 'util'; // To use promisify to handle callbacks
-
-const streamPipeline = promisify(pipeline);
 
 dotenv.config();
 
@@ -15,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 app.post('/synthesize-speech', async (req, res) => {
-    const { text, voice_settings } = req.body;
+    const { text } = req.body;
     const url = 'https://api.elevenlabs.io/v1/text-to-speech/5PL3mXw0y7D01sjTZoPz';
 
     const options = {
@@ -23,30 +18,29 @@ app.post('/synthesize-speech', async (req, res) => {
         headers: {
             'Accept': 'audio/mpeg',
             'Content-Type': 'application/json',
-            'xi-api-key': process.env.XI_API_KEY
+            'xi-api-key': process.env.API_KEY
         },
         body: JSON.stringify({
             text: text,
             model_id: 'eleven_multilingual_v2',
-            voice_settings: voice_settings
+            voice_settings: {
+                stability: 0.5,
+                similarity_boost: 0.5
+            }
         })
     };
 
     try {
         const response = await fetch(url, options);
         if (response.ok) {
-            // You might want to save the audio or send it directly to the client
-            // Example: Save the audio file
-            const audioFilePath = `audio-${Date.now()}.mp3`;
-            await streamPipeline(response.body, fs.createWriteStream(audioFilePath));
-            res.status(200).json({ message: 'Audio saved successfully!', path: audioFilePath });
+            res.setHeader('Content-Type', 'audio/mpeg');
+            response.body.pipe(res);
         } else {
-            // If response is not okay, send an error
             throw new Error(`API responded with status ${response.status}`);
         }
     } catch (error) {
-        console.error('Error handling the audio data:', error);
-        res.status(500).json({ error: 'Failed to process audio data', details: error.message });
+        console.error('Error fetching from ElevenLabs API:', error);
+        res.status(500).json({ error: 'Failed to fetch from ElevenLabs API', details: error.message });
     }
 });
 
